a9dd12dae858f68beaff78cfe600c75af7b4f183
Mohammad Haris Zia <mhzxbox360@gmail.com>
2023-06-28 01:43:35 +0500

    Changes in cron job and insight controller to send notifications

--- utils/cronJob.js
+++ utils/cronJob.js
@@ -1,6 +1,10 @@
 const cron = require('node-cron');
 
 const Insight = require('../models/Insight');
+const Reward = require('../models/Reward');
+const Notification = require('../models/Notification');
+const mongoose = require('mongoose');
+const fetch = require('node-fetch');
 
 exports.default = cron.schedule('0 0 * * *', async () => {
   const currentDate = new Date();
@@ -9,8 +13,67 @@ exports.default = cron.schedule('0 0 * * *', async () => {
   });
 
   for (const insight of expiredInsights) {
-    insight.status = 'completed';
-    await insight.save();
+    if (
+      insight.submissions >= insight.minParticipants &&
+      insight.status === 'active'
+    ) {
+      try {
+        const rewards = await Reward.find({
+          survey: insight?._id,
+        });
+
+        let notification;
+
+        for (const reward of rewards) {
+          notification = await Notification.findOne({
+            user: reward?.user,
+          });
+
+          await fetch(`${process.env.URL}api/v1/notification/send`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              title: `Your Reward for ${insight?.title} is ready`,
+              body: 'Click here to view reward',
+              user: reward?.user,
+              tokenID: notification?.tokenID,
+              image: null,
+              data: 'test',
+              navigate: 'Rewards',
+              id: mongoose.Types.ObjectId().valueOf(),
+            }),
+          });
+        }
+
+        notification = await Notification.findOne({
+          user: insight?.user,
+        });
+
+        await fetch(`${process.env.URL}api/v1/notification/send`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            title: `${insight?.title} survey has ended`,
+            body: 'Click here to view survey',
+            user: insight?.user,
+            tokenID: notification?.tokenID,
+            image: null,
+            data: 'test',
+            navigate: 'Survey',
+            id: mongoose.Types.ObjectId().valueOf(),
+          }),
+        });
+
+        insight.status = 'completed';
+        await insight.save();
+      } catch (err) {
+        console.log(err);
+      }
+    }
   }
   console.log('Insights updated');
 });
